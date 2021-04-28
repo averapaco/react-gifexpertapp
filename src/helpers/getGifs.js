36a96592e9dd8ba1bf85d0587cbe7b9188b86e61
Alexander Willam <averapaco@gmail.com>
2021-04-27 21:35:16 -0400

    Fix https git page

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,5 +1,5 @@
 export const getGifs = async(categoria) => {
-  const url = `http://api.giphy.com/v1/gifs/search?q=${encodeURI(categoria)}&limit=10&api_key=iCo216ChLYmawd0CrumOASEvbcl3WMP7`;
+  const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(categoria)}&limit=10&api_key=iCo216ChLYmawd0CrumOASEvbcl3WMP7`;
   const resp = await fetch(url);
   const {data} = await resp.json();
   const gifs = data.map( img => {
